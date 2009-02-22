bf14f01b2afff0bd66d27923bebe1272af4c18be
nonomonomusic <nonomonomusic@fd5aab1e-e55a-11dd-9ba5-89a75009fd5d>
2009-02-22 06:25:49 +0000

    git-svn-id: http://lazerdoom.googlecode.com/svn/trunk@12 fd5aab1e-e55a-11dd-9ba5-89a75009fd5d

--- lazerdoom/lazerdoom/lazerdoom.java
+++ lazerdoom/lazerdoom/lazerdoom.java
@@ -1,61 +1,47 @@
 package lazerdoom;
+import com.trolltech.qt.core.Qt.DockWidgetArea;
 import com.trolltech.qt.gui.*;
 import com.trolltech.qt.sql.QSqlTableModel;
 import java.math.*;
 import java.util.*;
 
-import sequence.GenericTimelineSequence;
+import patch.scene.PatchScene;
+import patch.view.PatchView;
 
-public class lazerdoom extends QWidget{
+import sequence.GenericTimelineSequence;
+import sequence.view.GenericSequenceEditor;
+import sequence.view.types.DoublePointSequenceEditor;
+
+public class lazerdoom extends QMainWindow{
 
     public static void main(String[] args) {
-        /*QApplication.initialize(args);
+        QApplication.initialize(args);
+        QApplication.setStyle(new QPlastiqueStyle());
 
         lazerdoom testlazerdoom = new lazerdoom(null);
         
         testlazerdoom.show();
 
         QApplication.exec();
-        */
-    	
-    	int numEntries = 100000;
-    	GenericTimelineSequence<Double> gs = new GenericTimelineSequence<Double>(); 
-    	
-    	long stIn = System.nanoTime();
-    	
-    	for(int i = 0; i< numEntries; i++) {
-    		gs.insert((long)(1000000*Math.random()), Math.random());
-    	}
-    	
-    	long eIn = System.nanoTime() - stIn;
-    	System.out.println("Insert Time msecs ("+numEntries+" Entries): "+eIn/(1000*1000));
-    	System.out.println("Insert entries/msecs:"+ (float)(numEntries/(eIn/(1000*1000))));
-    	
-    	long stGet = System.nanoTime();
-    	
-    	for(int i = 0; i< numEntries; i++) {
-    		gs.getAt((long)(1000000*Math.random()));
-    	}
-    	
-    	long eGet = System.nanoTime() - stGet;
-    	
-    	System.out.println("Get Time (random) msecs ("+numEntries+" Entries): "+eGet/(1000*1000));
-    	System.out.println("Get entries/msecs (random):"+ (float)(numEntries/(eGet/(1000*1000))));
-    	
-    	
-    	long stGetS = System.nanoTime();
-    	
-    	for(int i = 0; i< numEntries; i++) {
-    		gs.getAt((long)(1000000*(i/numEntries)));
-    	}
-    	
-    	long eGetS = System.nanoTime() - stGetS;
-    	
-    	System.out.println("Get Time (sequential) msecs ("+numEntries+" Entries): "+eGetS/(1000*1000));
-    	System.out.println("Get entries/msecs (sequential):"+ (float)(numEntries/(eGetS/(1000*1000))));
+
     }
 
     public lazerdoom(QWidget parent){
         super(parent);
+        this.setWindowTitle("lazerdoom 1000");
+        
+        PatchScene patchScene = new PatchScene();
+        PatchView patchView = new PatchView(patchScene);
+        QDockWidget patchViewDock = new QDockWidget(this);
+        patchViewDock.setWindowTitle("Patch");
+        patchViewDock.setWidget(patchView);
+        this.addDockWidget(DockWidgetArea.LeftDockWidgetArea, patchViewDock);
+        
+        DoublePointSequenceEditor testGenericSequenceEditor = new DoublePointSequenceEditor();
+        QDockWidget sequenceEditorDock = new QDockWidget(this);
+        sequenceEditorDock.setWindowTitle("Editor");
+        sequenceEditorDock.setWidget(testGenericSequenceEditor);
+        this.addDockWidget(DockWidgetArea.RightDockWidgetArea, sequenceEditorDock);
+        
     }
 }
